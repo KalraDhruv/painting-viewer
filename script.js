2c01100e3369191dda720508187dc2d941d21f29
KalraDhruv <dhruvkalra@proton.me>
2024-07-06 21:48:47 +0000

    Added event listener for clicks

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 document.addEventListener('DOMContentLoaded',function(){
     const data = JSON.parse(content);
     const thumbnail = document.getElementById('paintings').querySelector('ul'); 
+    const figure = document.querySelector('figure');
+    
+    // Event Handler for handling image clicked
+    thumbnail.addEventListener("click",(e)=>{
+	if(e.target && e.target.nodeName == "IMG") {
+	    figure.text
+	    
+	    console.log('You clicked image with id=' + e.target.dataset.id);
+
+	}
+    })
+
+    // Emptying the content inside thumbnail
     thumbnail.textContent = '';
 
+    // Loop for making thumbnails and adding id to each image.
     data.forEach((obj) =>{
 	const li= document.createElement('li');
+
 	imageLocation = 'images/small/'+obj.id+'.jpg';
 	const img =document.createElement('img');	
+
+	img.dataset.id = obj.id;
 	img.src=imageLocation;
+
 	li.appendChild(img);
 	thumbnail.appendChild(li);
+
 	console.log(imageLocation);
     })
 
