587e2d284b5c2126c6b1a28dc75e60fda521b4f9
KalraDhruv <dhruvkalra@proton.me>
2024-07-06 21:23:54 +0000

    Added thumbnail images to the website

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-/* add your code here */
+document.addEventListener('DOMContentLoaded',function(){
+    const data = JSON.parse(content);
+    const thumbnail = document.getElementById('paintings').querySelector('ul'); 
+    thumbnail.textContent = '';
+
+    data.forEach((obj) =>{
+	const li= document.createElement('li');
+	imageLocation = 'images/small/'+obj.id+'.jpg';
+	const img =document.createElement('img');	
+	img.src=imageLocation;
+	li.appendChild(img);
+	thumbnail.appendChild(li);
+	console.log(imageLocation);
+    })
+
+
+   
+
+
+});
